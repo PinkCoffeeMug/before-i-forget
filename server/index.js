f1f1d951ee5ae793b37b84f5fa81e84e9a29a1a2
Sarah Lin <xiaowei.lin@outlook.com>
2022-06-16 17:20:31 -0400

    db setup and connection

--- server/index.js
+++ server/index.js
@@ -7,4 +7,19 @@ const app = express();
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
-app. use(cors());
+app.use(cors());
+
+// https://www.mongodb.com/cloud/atlas
+
+//url
+const CONNECTION_URL = 'mongodb+srv://procrasprincess:procrasprincess123@cluster0.pmpvpfu.mongodb.net/?retryWrites=true&w=majority';
+
+//port
+const PORT = process.env.PORT || 5000;
+
+//connect mongo db and make sure console runs smoothly
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+//mongoose.set('useFindAndModify', false);
